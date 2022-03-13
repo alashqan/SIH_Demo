b54c568621233271d1c23ddab38136a067e8fe71
Al Ashqan Babu <80443704+alashqan@users.noreply.github.com>
2022-03-13 08:24:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+function view_mumbai()
+{
+    document.getElementById('mumbai').style.display = "block";
+    document.getElementById('karnataka').style.display = "none";
+    document.getElementById('mangalore').style.display = "none";
+    document.getElementById('karwar').style.display = "none";
+    document.getElementById('murdeshwar').style.display = "none";
+    document.getElementById('adyar').style.display = "none";
+}
+
+function view_karnataka()
+{
+    document.getElementById('mumbai').style.display = "none";
+    document.getElementById('karnataka').style.display = "block";
+    document.getElementById('mangalore').style.display = "none";
+    document.getElementById('karwar').style.display = "none";
+    document.getElementById('murdeshwar').style.display = "none";
+    document.getElementById('adyar').style.display = "none";
+}
+
+function view_mangalore()
+{
+    document.getElementById('mumbai').style.display = "none";
+    document.getElementById('karnataka').style.display = "none";
+    document.getElementById('mangalore').style.display = "block";
+    document.getElementById('karwar').style.display = "none";
+    document.getElementById('murdeshwar').style.display = "none";
+    document.getElementById('adyar').style.display = "none";
+}
+
+function view_karwar()
+{
+    document.getElementById('mumbai').style.display = "none";
+    document.getElementById('karnataka').style.display = "none";
+    document.getElementById('mangalore').style.display = "none";
+    document.getElementById('karwar').style.display = "block";
+    document.getElementById('murdeshwar').style.display = "none";
+    document.getElementById('adyar').style.display = "none";
+}
+
+function view_murdeshwar()
+{
+    document.getElementById('mumbai').style.display = "none";
+    document.getElementById('karnataka').style.display = "none";
+    document.getElementById('mangalore').style.display = "none";
+    document.getElementById('karwar').style.display = "none";
+    document.getElementById('murdeshwar').style.display = "block";
+    document.getElementById('adyar').style.display = "none";
+}
+
+
+function view_adyar()
+{
+    document.getElementById('mumbai').style.display = "none";
+    document.getElementById('karnataka').style.display = "none";
+    document.getElementById('mangalore').style.display = "none";
+    document.getElementById('karwar').style.display = "none";
+    document.getElementById('murdeshwar').style.display = "none";
+    document.getElementById('adyar').style.display = "block";
+}
+
